aaed267d042c4c1cdeb1ca33eca1e4ab4b2791ed
GeodesicCarbon <vadim.gurkalov@gmail.com>
2017-02-02 18:38:45 +0200

    Fixed whitespaces

--- Rasp_Studio_0.1.1.py
+++ Rasp_Studio_0.1.1.py
@@ -36,58 +36,58 @@ GPIO.setup(relay_2c_pin, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(relay_2d_pin, GPIO.OUT, initial=GPIO.HIGH)
 
 def power_off(channel):# Turning main power off sequentially in reverse order
-	global power_flag
-	if sub_flag: # Turning sub power off if on
-		print "Turning off sub power first"
-		sleep(2.5)
-		sub_off(main_sw_pin)
-	GPIO.output(relay_1c_pin, GPIO.HIGH)
-	time.sleep(power_delay)
-	GPIO.output(relay_1b_pin, GPIO.HIGH)
-	time.sleep(power_delay)
-	GPIO.output(relay_1a_pin, GPIO.HIGH)
-	GPIO.output(led_pin, GPIO.LOW) # Led on
-	power_flag = 0
-	print "Main power turned off"
+    global power_flag
+    if sub_flag: # Turning sub power off if on
+        print "Turning off sub power first"
+        sleep(2.5)
+        sub_off(main_sw_pin)
+    GPIO.output(relay_1c_pin, GPIO.HIGH)
+    time.sleep(power_delay)
+    GPIO.output(relay_1b_pin, GPIO.HIGH)
+    time.sleep(power_delay)
+    GPIO.output(relay_1a_pin, GPIO.HIGH)
+    GPIO.output(led_pin, GPIO.LOW) # Led on
+    power_flag = 0
+    print "Main power turned off"
 
 def power_on(channel):
     print "Toggling main power"
     global power_flag
-	GPIO.output(relay_1a_pin, GPIO.LOW)
-	time.sleep(power_delay)
-	GPIO.output(relay_1b_pin, GPIO.LOW)
-	time.sleep(power_delay)
-	GPIO.output(relay_1c_pin, GPIO.LOW)
-	GPIO.output(led_pin, GPIO.HIGH)
-	power_flag = 1 # Led off
-	print "Main power turned on"
+    GPIO.output(relay_1a_pin, GPIO.LOW)
+    time.sleep(power_delay)
+    GPIO.output(relay_1b_pin, GPIO.LOW)
+    time.sleep(power_delay)
+    GPIO.output(relay_1c_pin, GPIO.LOW)
+    GPIO.output(led_pin, GPIO.HIGH)
+    power_flag = 1 # Led off
+    print "Main power turned on"
 
 def sub_off(channel):
     global sub_flag # Turning sub power off sequentially in reverse order
-	print "Turning off sub power"
-	GPIO.output(relay_2a_pin, GPIO.HIGH)
-	time.sleep(power_delay)
-	GPIO.output(relay_2b_pin, GPIO.HIGH)
-	time.sleep(power_delay)
-	GPIO.output(relay_2c_pin, GPIO.HIGH)
-	time.sleep(power_delay)
-	GPIO.output(relay_2d_pin, GPIO.HIGH)
-	sub_flag = 1
-	print "Sub power turned off"
-		
+    print "Turning off sub power"
+    GPIO.output(relay_2a_pin, GPIO.HIGH)
+    time.sleep(power_delay)
+    GPIO.output(relay_2b_pin, GPIO.HIGH)
+    time.sleep(power_delay)
+    GPIO.output(relay_2c_pin, GPIO.HIGH)
+    time.sleep(power_delay)
+    GPIO.output(relay_2d_pin, GPIO.HIGH)
+    sub_flag = 1
+    print "Sub power turned off"
+        
 def sub_on(channel):
-	global sub_flag # Turning sub power on sequentially
-	if power_flag: 
-	print "Turning on sub power"
-	GPIO.output(relay_2d_pin, GPIO.LOW)
-	time.sleep(power_delay)
-	GPIO.output(relay_2c_pin, GPIO.LOW)
-	time.sleep(power_delay)
-	GPIO.output(relay_2b_pin, GPIO.LOW)
-	time.sleep(power_delay)
-	GPIO.output(relay_2a_pin, GPIO.LOW)
-	sub_flag = 0
-	print "Sub power turned on"
+    global sub_flag # Turning sub power on sequentially
+    if power_flag: 
+    print "Turning on sub power"
+    GPIO.output(relay_2d_pin, GPIO.LOW)
+    time.sleep(power_delay)
+    GPIO.output(relay_2c_pin, GPIO.LOW)
+    time.sleep(power_delay)
+    GPIO.output(relay_2b_pin, GPIO.LOW)
+    time.sleep(power_delay)
+    GPIO.output(relay_2a_pin, GPIO.LOW)
+    sub_flag = 0
+    print "Sub power turned on"
 
 GPIO.add_event_detect(main_sw_pin, GPIO.FALLING, callback=power_on, bouncetime=1000) # Set up triggers
 GPIO.add_event_detect(main_sw_pin, GPIO.RISING, callback=power_off, bouncetime=1000)
