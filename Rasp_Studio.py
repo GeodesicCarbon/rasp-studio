c92519c159aaa045ec36420ff99c5ae976f666b6
Vadim Gurkalov <vadim.gurkalov@gmail.com>
2017-02-06 20:35:05 +0000

    Debugging the Rasp side

--- Rasp_Studio.py
+++ Rasp_Studio.py
@@ -2,10 +2,11 @@
 # Version 0.1.2
 import RPi.GPIO as GPIO
 import serial
-ser = serial.Serial('/dev/tty', 9600) # IMPORTANT: change this to actual address
+import time
+ser = serial.Serial('/dev/ttyACM0', 9600) # IMPORTANT: change this to actual address
 GPIO.setmode(GPIO.BCM)
 
-print "Initializing, please wait ..."
+print ("Initializing, please wait ...")
 main_sw_pin = 25 # Pin for main switch
 sub_sw_pin = 24 # Pin for secondary switch
 
@@ -27,7 +28,7 @@ GPIO.setup(led_pin, GPIO.OUT) # Initializing led pin
 def power_off(channel):# Turning main power off sequentially in reverse order
     global power_flag
     if sub_flag: # Turning sub power off if on
-        print "Turning off sub power first"
+        print ("Turning off sub power first")
         sleep(2.5)
         sub_off(main_sw_pin)
     for pin in main_power_pins:
@@ -36,7 +37,9 @@ def power_off(channel):# Turning main power off sequentially in reverse order
         sleep(power_delay)
     GPIO.output(led_pin, GPIO.LOW) # Led off
     power_flag = 0
-    print "Main power turned off"
+    GPIO.remove_event_detect(25)
+    GPIO.add_event_detect(main_sw_pin, GPIO.FALLING, callback=power_on, bouncetime=1000)
+    print ("Main power turned off")
 
 def power_on(channel):
     print "Toggling main power"
@@ -46,40 +49,46 @@ def power_on(channel):
         ser.write('1')
         time.sleep(power_delay)
     power_flag = 1 # Led on
-    print "Main power turned on"
+    GPIO.remove_event_detect(main_sw_pin)
+    GPIO.add_event_detect(main_sw_pin, GPIO.RISING, callback=power_off, bouncetime=1000) # Set up triggers
+    print ("Main power turned on")
 
 def sub_off(channel):
     global sub_flag # Turning sub power off sequentially in reverse order
-    print "Turning off sub power"
+    print ("Turning off sub power")
     for pin in sub_power_pins:
         ser.write(chr(pin))
         ser.write('0')
         time.sleep(power_delay)
     sub_flag = 1
-    print "Sub power turned off"
+    GPIO.remove_event_detect(sub_sw_pin)
+    GPIO.add_event_detect(sub_sw_pin, GPIO.FALLING, callback=sub_on, bouncetime=1000)
+    print ("Sub power turned off")
 
 def sub_on(channel):
     global sub_flag # Turning sub power on sequentially
     if power_flag:
-    print "Turning on sub power"
-    for pin in sub_power_pins:
-        ser.write(chr(pin))
-        ser.write('1')
-        time.sleep(power_delay)
-    sub_flag = 0
-    print "Sub power turned on"
+        print ("Turning on sub power")
+        for pin in sub_power_pins:
+            ser.write(chr(pin))
+            ser.write('1')
+            time.sleep(power_delay)
+        sub_flag = 0
+        GPIO.remove_event_detect(sub_sw_pin)
+        GPIO.add_event_detect(sub_sw_pin, GPIO.RISING, callback=sub_off, bouncetime=1000)
+        print ("Sub power turned on")
 
 GPIO.add_event_detect(main_sw_pin, GPIO.FALLING, callback=power_on, bouncetime=1000) # Set up triggers
-GPIO.add_event_detect(main_sw_pin, GPIO.RISING, callback=power_off, bouncetime=1000)
-GPIO.add_event_detect(sub_sw_pin, GPIO.FALLING, callback=sub_on bouncetime=1000)
-GPIO.add_event_detect(sub_sw_pin, GPIO.RISING, callback=sub_off bouncetime=1000)
+# GPIO.add_event_detect(main_sw_pin, GPIO.RISING, callback=power_off, bouncetime=1000)
+# GPIO.add_event_detect(sub_sw_pin, GPIO.FALLING, callback=sub_on, bouncetime=1000)
+GPIO.add_event_detect(sub_sw_pin, GPIO.RISING, callback=sub_off, bouncetime=1000)
 
-print "Waiting for input ... "
+print ("Waiting for input ... ")
 
 try:
     while 1: # Keep idle and listen for input
         time.sleep(1)
 except KeyboardInterrupt:
-    print "Exiting ..."
+    print ("Exiting ...")
     GPIO.cleanup()
-    print "Have a nice day!"
+    print ("Have a nice day!")
